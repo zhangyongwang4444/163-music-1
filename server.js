1b0b18b2edf955a08dd70c982896d4eac235ee38
zhangyongwang <349368992@qq.com>
2018-08-23 23:14:47 +0800

    配置服务器

--- server.js
+++ server.js
@@ -26,15 +26,20 @@ var server = http.createServer(function (request, response) {
         response.setHeader('Content-Type', 'text/json;charset=utf-8')
         response.setHeader('Access-Control-Allow-Origin', '*')
 
-        var json = fs.readFileSync('./qiniu-key.json')
+        var config = fs.readFileSync('./qiniu-key.json')
+        config = JSON.parse(config)
 
-        var accessKey = 'your access key';
-        var secretKey = 'your secret key';
+        let { accessKey, secretKey } = config;
         var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 
+        var options = {
+            scope: '163-music-demo-1',
+        };
+        var putPolicy = new qiniu.rs.PutPolicy(options);
+        var uploadToken = putPolicy.uploadToken(mac);
         response.write(`
     {
-        "uptoken" : "${}"
+        "uptoken" : "${uploadToken}"
     }
     `)
         response.end()
