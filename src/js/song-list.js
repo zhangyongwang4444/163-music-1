e64f66e1fc327192c5b6f78351d05cf669b97d0b
zhangyongwang <349368992@qq.com>
2018-08-27 09:37:24 +0800

    点击歌曲时，激活歌曲项

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -8,7 +8,7 @@
             let $el = $(this.el)
             $el.html(this.template)
             let { songs } = data
-            let liList = songs.map((song) => $('<li></li>').text(song.name))
+            let liList = songs.map((song) => $('<li></li>').text(song.name).attr('data-song-id',song.id))
             $el.find('ul').empty()
             liList.map((domLi) => {
                 $el.find('ul').append(domLi)
@@ -57,6 +57,8 @@
         bindEvents() {
             $(this.view.el).on('click', 'li', (e) => {
                 this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id: songId})
             })
         },
         bindEventHub() {
