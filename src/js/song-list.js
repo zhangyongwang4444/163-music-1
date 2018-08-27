0b57ea59ddfc80a1e0303ab2be133caf935acd7b
zhangyongwang <349368992@qq.com>
2018-08-27 20:09:41 +0800

    解决视图状态丢失

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -7,19 +7,21 @@
         render(data) {
             let $el = $(this.el)
             $el.html(this.template)
-            let { songs } = data
-            let liList = songs.map((song) => $('<li></li>').text(song.name).attr('data-song-id', song.id))
+            let { songs, selectSongId } = data
+            let liList = songs.map((song) => {
+                let $li = $('<li></li>').text(song.name).attr('data-song-id', song.id)
+                if(song.id === selectSongId){
+                    $li.addClass('active')
+                }
+                return $li
+            })
             $el.find('ul').empty()
             liList.map((domLi) => {
                 $el.find('ul').append(domLi)
             })
             // $(this.el).html(this.template)
         },
-        activeItem(li) {
-            let $li = $(li)
-            $li.addClass('active')
-                .siblings('.active').removeClass('active')
-        },
+       
         clearActive() {
             $(this.el).find('.active').removeClass('active')
         }
@@ -27,7 +29,8 @@
 
     let model = {
         data: {
-            songs: []
+            songs: [],
+            selectSongId: undefined
         },
         find() {
             var query = new AV.Query('Song');
@@ -56,8 +59,12 @@
         },
         bindEvents() {
             $(this.view.el).on('click', 'li', (e) => {
-                this.view.activeItem(e.currentTarget)
+
                 let songId = e.currentTarget.getAttribute('data-song-id')
+
+                this.model.data.selectSongId = songId
+                this.view.render(this.model.data)
+
                 let data
                 let songs = this.model.data.songs
                 for (let i = 0; i < songs.length; i++) {
@@ -83,7 +90,7 @@
             })
             window.eventHub.on('update', (song) => {
                 let songs = this.model.data.songs
-               
+
                 for (let i = 0; i < songs.length; i++) {
                     if (songs[i].id === song.id) {
                         Object.assign(songs[i], song)
