52857cd52b0576a9fb9bd75405a2e8947c7fef3a
zhangyongwang <349368992@qq.com>
2018-08-28 17:00:59 +0800

    首页 tab 切换 --（渲染最新音乐歌单）

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -18,6 +18,8 @@
             this.view.init()
             this.model = model
             this.bindEvnetHub()
+            this.loadModule1()
+            this.loadModule2()
         },
         bindEvnetHub() {
             window.eventHub.on('selectTab', (tabName) => {
@@ -28,6 +30,22 @@
                 }
 
             })
+        },
+        loadModule1() {
+            let script1 = document.createElement('script')
+            script1.src = "./js/index/page-1-1.js"
+            script1.onload = function () {
+                console.log('模块1加载完毕')
+            }
+            document.body.appendChild(script1)
+        },
+        loadModule2() {
+            let script2 = document.createElement('script')
+            script2.src = "./js/index/page-1-2.js"
+            script2.onload = function () {
+                console.log('模块2加载完毕')
+            }
+            document.body.appendChild(script2)
         }
     }
     controller.init(view, model)
