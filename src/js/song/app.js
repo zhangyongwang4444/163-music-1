351203bd1d657ea0a855978b406071395ce8e2af
zhangyongwang <349368992@qq.com>
2018-08-29 10:05:50 +0800

    播放和暂停

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,5 +1,26 @@
 {
-    let view = {}
+    let view = {
+        el: '#app',
+        template: `
+        <audio  src={{url}}></audio>
+        <div>
+            <button class="play">播放</button>
+            <button class="pause">暂停</button>
+        </div>
+        `,
+        render(data) {
+            $(this.el).html(this.template.replace('{{url}}', data.url))
+
+        },
+        play() {
+            let audio = $(this.el).find('audio')[0]
+            audio.play()
+        },
+        pause() {
+            let audio = $(this.el).find('audio')[0]
+            audio.pause()
+        }
+    }
     let model = {
         data: {
             id: '',
@@ -21,9 +42,20 @@
             this.view = view
             this.model = model
             let id = this.getSongId()
-            
+
             this.model.get(id).then(() => {
-                console.log(this.model.data)
+                this.view.render(this.model.data)
+            }),
+                this.bindEvents()
+        },
+        bindEvents() {
+            $(this.view.el).on('click', '.play', () => {
+                console.log('播放')
+                this.view.play()
+            })
+            $(this.view.el).on('click', '.pause', () => {
+                console.log('暂停')
+                this.view.pause()
             })
         },
         getSongId() {
